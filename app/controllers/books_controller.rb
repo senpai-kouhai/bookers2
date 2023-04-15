182182bb2994f83b4482528c91f2361a65268fce
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-15 13:11:05 +0000

    [MVC ok]4-27

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
